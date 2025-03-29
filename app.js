25ab1e8cb543cc5e28b1cc9e71189518f585f96f
Shirley C <sshirleyy.chen@mail.utoronto.ca>
2025-03-29 01:35:28 -0400

    scroll bheaviour

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-})
-
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+});
